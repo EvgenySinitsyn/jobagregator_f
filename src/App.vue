2eb586fbbb2fa5f68260192d03e6d01ef5270274
e.sinitsyn <e.sinitsyn@def.team>
2025-01-21 21:12:54 +0300

    Добавлена страница поиска вакансий и страница подсчета статистики

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,129 @@
 <template>
-    <div id="app">
-      <ResumePage />
-    </div>
-  </template>
+  <div id="app">
+    <header>
+      <h1>Jobagregator</h1>
+      <nav>
+        <span @click="changeComponent('resume')" :class="{ active: currentComponentName == 'resume' }">Резюме</span>
+        <span @click="changeComponent('vacancies')" :class="{ active: currentComponentName == 'vacancies' }">Вакансии</span>
+        <span @click="changeComponent('stat')" :class="{ active: currentComponentName == 'stat' }">Статистика</span>
+        <a href="#">Вход</a>
+        <a href="#">Регистрация</a>
+        <a href="#">О проекте</a>
+      </nav>
+    </header>
 
-  <script>
-  import ResumePage from './components/ResumePage.vue';
+    <main>
+      <component :is="currentComponent"></component>
+    </main>
+  </div>
+</template>
 
-  export default {
-    components: {
-      ResumePage
-    }
+<script>
+import ResumePage from './components/ResumePage.vue';
+import VacancyPage from './components/VacancyPage.vue';
+import StatPage from './components/StatPage.vue';
+
+export default {
+  data() {
+  return {
+    currentComponentName: 'resume',
+    currentComponent: ResumePage // или любой другой компонент по умолчанию
   };
-  </script>
-  
+},
+methods: {
+  changeComponent(component) {
+    switch (component) {
+      case 'resume':
+        this.currentComponentName = 'resume';
+        this.currentComponent = ResumePage;
+        break;
+      case 'vacancies':
+        this.currentComponentName = 'vacancies';
+        this.currentComponent = VacancyPage;
+        break;
+      case 'stat':
+        this.currentComponentName = 'stat';
+        this.currentComponent = StatPage;
+        break;
+      default:
+        this.currentComponent = null;
+    }
+  }
+}
+};
+</script>
+
+<style>
+body {
+  font-family: Arial, sans-serif;
+  margin: 0;
+  padding: 0;
+  padding-top: 60px;
+  min-height: 720px;
+  background: linear-gradient(to bottom, #1b1b2c, #843021);
+  color: #ffffff;
+}
+
+header {
+  position: fixed; /* Фиксируем заголовок */
+  top: 0; /* Устанавливаем его в верхней части экрана */
+  left: 0; /* Устанавливаем его в левом углу */
+  right: 0; /* Устанавливаем его в правом углу */
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: rgba(30, 30, 30, 0.8);
+  color: white;
+  padding: 10px 20px;
+  border-bottom: 2px solid #444;
+  z-index: 1000; /* Устанавливаем высокий z-index, чтобы заголовок был поверх других элементов */
+}
+
+
+
+.main_logo {
+  padding-left: 10%;
+  height: 30px;
+  width: auto;
+}
+
+header h1 {
+  margin: 0;
+}
+
+nav a {
+  color: white;
+  text-decoration: none;
+  margin-left: 15px;
+  padding: 5px 10px;
+  border-radius: 4px;
+  transition: background-color 0.3s;
+}
+
+nav a:hover {
+  background-color: #333;
+}
+
+nav a:active {
+  background-color: #333;
+}
+
+nav span {
+  color: white;
+  text-decoration: none;
+  margin-left: 15px;
+  padding: 5px 10px;
+  border-radius: 4px;
+  transition: background-color 0.3s;
+  cursor: pointer;
+}
+
+nav span:hover {
+  background-color: #333;
+}
+
+nav span.active {
+  background-color: #333;
+}
+</style>
+
--- src/components/ResumePage.vue
+++ src/components/ResumePage.vue
@@ -1,18 +1,10 @@
 <template>
 
-  <body>
-    <header>
-      <h1>Jobagregator</h1>
-      <nav>
-        <a href="#">Вход</a>
-        <a href="#">Регистрация</a>
-        <a href="#">О проекте</a>
-      </nav>
-    </header>
-
     <div class="container">
       <aside class="sidebar">
-        <h2>Фильтры</h2>
+        <label for="position">Должность:</label>
+        <input type="text" id="position" v-model="text" placeholder="Введите должность">
+
         <label for="city">Город:</label>
         <input type="text" id="city" v-model="city" placeholder="Введите город">
 
@@ -29,12 +21,6 @@
         <label for="experienceFrom">Опыт (от):</label>
         <input type="number" id="experienceFrom" v-model="experienceFrom" min="0" max="120" placeholder="От (мес)">
 
-        <label for="experienceTo">Опыт (до):</label>
-        <input type="number" id="experienceTo" v-model="experienceTo" min="0" max="120" placeholder="До (мес)">
-
-        <label for="position">Должность:</label>
-        <input type="text" id="position" v-model="text" placeholder="Введите должность">
-
         <label for="education">Образование:</label>
         <select id="education" v-model="education">
           <option value="">Выберите образование</option>
@@ -49,13 +35,15 @@
         <label for="ageTo">Возраст (до):</label>
         <input type="number" id="ageTo" v-model="ageTo" min="18" max="100" placeholder="До (лет)">
 
-        <button class="button" @click="fetchResumes()">Найти</button>
-        <div v-if="loading" class="loading-message">Загрузка...</div>
-        <div v-if="error" class="text-danger">{{ error }}</div>
+        <button class="button" @click="clearFetchResumes()">
+          <span v-if="!loading">Найти</span>
+          <span v-else class="loader"></span>
+        </button>
+
       </aside>
 
       <main class="main">
-        
+
         <div class="resume" v-for="resume in resumes" :key="resume.id">
 
           <h3>{{ resume.profession.__data__.name }}</h3>
@@ -66,16 +54,20 @@
           <p><strong>Пол:</strong> {{ gender_dict[resume.sex] }}</p>
           <p v-if="resume.salary_from"><strong>Зарплатные ожидания от:</strong> {{ resume.salary_from }} {{
             resume.currency }}</p>
-          <p><strong>Опыт работы (в месяцах):</strong> {{ resume.experience_months }}</p>
-          <!-- <p><strong><a :href="resume.link" target="_blank">Ссылка на резюме</a></strong></p> -->
+          <p v-if="resume.experience_months"><strong>Опыт работы (в месяцах):</strong> {{ resume.experience_months }}
+          </p>
           <a :href="resume.link" target="_blank" class="platform-button">
             <img :src="platform_img_dict[resume.platform.__data__.name]" alt=""><span class="arrow-right"></span>
           </a>
         </div>
+        <div v-if="error" class="text-danger">{{ error }}</div>
+        <button v-if="resumes" class="button" @click="loadMore()">
+                <span v-if="!loading">Найти далее</span>
+                <span v-else class="loader"></span>
+            </button>
       </main>
     </div>
 
-  </body>
 </template>
 
 <script>
@@ -109,6 +101,7 @@ export default {
       resumes: [],
       loading: false,
       error: null,
+      currentPage: 0
     };
   },
 
@@ -125,25 +118,33 @@ export default {
       });
     },
 
-    async fetchResumes() {
+    async clearFetchResumes() {
+      this.resumes = [];
+      window.scrollTo(0, 0);
+      this.currentPage = 0;
+      this.fetchResumes(this.currentPage);
+    },
+
+    async fetchResumes(page = 1) {
       this.loading = true;
       this.error = null;
 
       const params = {
-        city: this.city,
-        gender: this.gender,
-        create_tm: this.create_tm,
-        experience_from: this.experienceFrom,
-        experience_to: this.experienceTo,
-        position: this.text,
-        education: this.education,
-        age_from: this.ageFrom,
-        age_to: this.ageTo,
+        ...(this.city && this.city.trim() && { city: this.city }),
+        ...(this.gender && this.gender.trim() && { gender: this.gender }),
+        ...(this.create_tm && this.create_tm.trim() && { create_tm: this.create_tm }),
+        ...(this.experienceFrom !== null && this.experienceFrom !== '' && { experience_from: this.experienceFrom }),
+        ...(this.experienceTo !== null && this.experienceTo !== '' && { experience_to: this.experienceTo }),
+        ...(this.text && this.text.trim() && { position: this.text }),
+        ...(this.education && this.education.trim() && { education: this.education }),
+        ...(this.ageFrom !== null && this.ageFrom !== '' && { age_from: this.ageFrom }),
+        ...(this.ageTo !== null && this.ageTo !== '' && { age_to: this.ageTo }),
+        page,
       };
 
       try {
         const response = await axios.get('http://localhost:8080/resumes', { params });
-        this.resumes = response.data;
+        this.resumes = [...this.resumes, ...response.data];
       } catch (err) {
         console.error('Ошибка при получении данных:', err);
         if (err.response) {
@@ -157,6 +158,28 @@ export default {
         this.loading = false;
       }
     },
+    // handleScroll() {
+    //   const scrollTop = window.scrollY;
+    //   const windowHeight = window.innerHeight;
+    //   const documentHeight = document.documentElement.scrollHeight;
+
+    //   if (scrollTop + windowHeight >= documentHeight - 10 && !this.loading) {
+    //     this.loadMore();
+    //   }
+    // },
+    loadMore() {
+      this.currentPage++;
+      if (!this.loading) {
+        this.fetchResumes(this.currentPage);
+      }
+    },
+  },
+  mounted() {
+    // window.addEventListener('scroll', this.handleScroll);
+    this.fetchResumes();
+  },
+  beforeDestroy() {
+    window.removeEventListener('scroll', this.handleScroll);
   },
 };
 
@@ -167,10 +190,9 @@ body {
   font-family: Arial, sans-serif;
   margin: 0;
   padding: 0;
-  background: linear-gradient(to bottom, #000000, #ffffff);
-  /* Черно-белый градиент */
+  min-height: 720px;
+  background: linear-gradient(to bottom, #1b1b2c, #843021);
   color: #ffffff;
-  /* Белый текст */
 }
 
 header {
@@ -178,17 +200,15 @@ header {
   justify-content: space-between;
   align-items: center;
   background-color: rgba(30, 30, 30, 0.8);
-  /* Полупрозрачный темно-серый фон для заголовка */
   color: white;
   padding: 10px 20px;
   border-bottom: 2px solid #444;
-  /* Разделительная линия */
 }
 
 .main_logo {
-    padding-left: 10%;
-    height: 30px; /* Устанавливаем высоту изображения на 100% */
-    width: auto; /* Сохраняем пропорции изображения */
+  padding-left: 10%;
+  height: 30px;
+  width: auto;
 }
 
 header h1 {
@@ -206,7 +226,6 @@ nav a {
 
 nav a:hover {
   background-color: #333;
-  /* Темнее при наведении */
 }
 
 .container {
@@ -217,71 +236,59 @@ nav a:hover {
 }
 
 .sidebar {
+  position: fixed;
+  top: 79px;
+  left: 10%;
   width: 250px;
   background-color: rgba(30, 30, 30, 0.9);
-  /* Полупрозрачный темно-серый фон для бокового меню */
   padding: 20px;
   box-shadow: 0 2px 5px rgba(0, 0, 0, 0.5);
   border-radius: 5px;
-  /* Скругленные углы для бокового меню */
+  z-index: 1000;
 }
 
-.sidebar h2 {
-  margin-top: 0;
-  color: #ffffff;
-  /* Белый цвет для заголовка бокового меню */
-}
 
 input,
 select {
   width: calc(100% - 10px);
-  /* Ширина полей ввода */
   padding: 8px;
-  margin-bottom: 15px;
+  margin-bottom: 7px;
   border: 1px solid #444;
-  /* Темная рамка */
   background-color: #333;
-  /* Темный фон для полей ввода и селектов */
   color: #ffffff;
-  /* Белый текст */
   box-sizing: border-box;
-  /* Учитывать отступы и границы в ширине */
 }
 
 .button {
   background-color: #444;
-  /* Темно-серая кнопка */
   color: white;
   border: none;
-  padding: 10px;
+  margin-top: 7px;
+  padding: 10px 20px;
   border-radius: 4px;
-  /* Скругленные углы для кнопки */
   cursor: pointer;
   transition: background-color 0.3s;
-  width: 100%;
+  width: 97%;
+  height: 40px;
+  display: inline-block;
 }
 
 .button:hover {
   background-color: #555;
-  /* Темнее при наведении на кнопку */
 }
 
 .main {
   flex-grow: 1;
   padding: 20px;
   background-color: rgba(30, 30, 30, 0.9);
-  /* Полупрозрачный темно-серый фон для основного блока */
   box-shadow: 0 2px 5px rgba(0, 0, 0, 0.5);
-  margin-left: 20px;
+  margin-left: 260px;
   border-radius: 5px;
-  /* Скругленные углы для основного блока */
 }
 
 .resume {
   background-color: #333;
-  /* Темный фон для резюме */
   border-radius: 5px;
-  /* Скругленные углы для резюме */
   padding: 15px;
   margin-bottom: 15px;
   box-shadow: 0 1px 3px rgba(0, 0, 0, 0.5);
@@ -290,25 +297,15 @@ select {
 
 .platform-button {
   position: absolute;
-  /* Абсолютное позиционирование для кнопки */
   bottom: 10px;
-  /* Отступ сверху */
   right: 10px;
-  /* Отступ справа */
   display: inline-flex;
-  /* Используем flex для выравнивания */
   align-items: center;
-  /* Центрируем элементы по вертикали */
   background-color: rgba(3, 94, 4, 0.514);
-  /* Серый фон для кнопки */
   color: white;
-  /* Белый цвет текста */
   padding: 10px 15px;
-  /* Отступы внутри кнопки */
   border-radius: 5px;
-  /* Скругленные углы для кнопки */
   text-decoration: none;
-  /* Убираем подчеркивание у ссылки */
 }
 
 .arrow-right {
@@ -316,17 +313,33 @@ select {
   width: 0;
   height: 0;
   border-top: 5px solid transparent;
-  /* Верхняя часть стрелки */
   border-bottom: 5px solid transparent;
-  /* Нижняя часть стрелки */
   border-left: 8px solid rgb(82, 82, 82);
-  /* Правая часть стрелки (зеленая) */
   margin-left: 5px;
-  /* Отступ между текстом и стрелкой */
 }
 
 .platform-button:hover {
   transition: background-color 0.3s;
-  background-color: rgba(3, 94, 4, 0.7); /* Светлее при наведении */
+  background-color: rgba(3, 94, 4, 0.7);
+}
+
+.loader {
+  border: 3px solid #f3f3f3;
+  border-top: 3px solid #3498db;
+  border-radius: 50%;
+  width: 20px;
+  height: 20px;
+  animation: spin 0.8s linear infinite;
+  display: inline-block;
+}
+
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+  }
+
+  100% {
+    transform: rotate(360deg);
+  }
 }
 </style>
